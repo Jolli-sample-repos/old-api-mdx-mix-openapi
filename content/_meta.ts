6783e544c8eac2b0cbff0ac04b78547a08a137f7
Aidan <230012360+aidancrosbie@users.noreply.github.com>
2025-12-11 17:30:59 -0800

    Update documentation (preserve custom files)

--- content/_meta.ts
+++ content/_meta.ts
@@ -1,5 +1,5 @@
 export default {
-  'index': 'Home',
+  'index': { display: 'hidden' },
   'valid-changelog': 'Valid - Changelog',
   'valid-troubleshooting': 'Valid - Troubleshooting',
   'valid-advanced-features': 'Valid - Advanced Features',
@@ -23,5 +23,15 @@ export default {
     title: 'Valid - Pet Store API (JSON)',
     type: 'page',
     href: '/api-docs-valid-pet-store-api-json.html'
+  },
+  'api-reference': {
+    title: 'API Reference',
+    type: 'menu',
+    items: {
+      'valid-jolli-api-yaml': { title: 'Valid - Jolli API (YAML)', href: '/api-docs/valid-jolli-api-yaml' },
+      'valid-pet-store-api-yaml': { title: 'Valid - Pet Store API (YAML)', href: '/api-docs/valid-pet-store-api-yaml' },
+      'valid-jolli-api-json': { title: 'Valid - Jolli API (JSON)', href: '/api-docs/valid-jolli-api-json' },
+      'valid-pet-store-api-json': { title: 'Valid - Pet Store API (JSON)', href: '/api-docs/valid-pet-store-api-json' }
+    }
   }
 }
